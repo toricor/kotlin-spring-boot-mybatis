8cfde8258a83ce8c6a279048ad464374087fb494
toriyabe <toriyabe@mfpc243.local>
2017-10-04 08:36:36 +0900

    schema.sql追加

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS user (
+    id       INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name     VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS event (
+    id               INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title            VARCHAR(255) NOT NULL,
+    description      VARCHAR(255) NOT NULL,
+    datetime         DATETIME NOT NULL,
+    publish          DATETIME NOT NULL,
+    author           INTEGER NOT NULL,
+    place            VARCHAR(255) NOT NULL,
+    participants     VARCHAR(255) NOT NULL,
+    max_participants VARCHAR(255) NOT NULL,
+    FOREIGN KEY(author) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS reservation (
+    id        INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id   INTEGER NOT NULL,
+    event_id  INTEGER NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES user(id),
+    FOREIGN KEY(event_id) REFERENCES event(id)
+);
