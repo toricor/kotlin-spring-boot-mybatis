bfa0a7536a374fe076d577405b370f6b082b278f
imomochi/toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-11-20 15:57:13 +0900

    Update TrainingApplication.kt

--- src/main/kotlin/com/toricor/training/TrainingApplication.kt
+++ src/main/kotlin/com/toricor/training/TrainingApplication.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class TrainingApplication
 
+// added comment
 fun main(args: Array<String>) {
     SpringApplication.run(TrainingApplication::class.java, *args)
 }
