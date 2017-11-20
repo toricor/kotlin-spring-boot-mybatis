bfd464be1f5a693ab125411d671a62d6d2567317
imomochi/toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-11-20 15:20:02 +0900

    add comment

--- src/test/kotlin/com/toricor/training/TrainingApplicationTests.kt
+++ src/test/kotlin/com/toricor/training/TrainingApplicationTests.kt
@@ -8,6 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class TrainingApplicationTests {
+	// hogehoge
 	@Test
 	fun contextLoads() {
 	}
